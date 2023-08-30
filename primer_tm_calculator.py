756912027536eb00ff79798f6cd545795eef3096
shinepreventer <53377241+shinepreventer@users.noreply.github.com>
2023-08-30 22:22:45 +0800

    Update primer_tm_calculator.py

--- primer_tm_calculator.py
+++ primer_tm_calculator.py
@@ -1,39 +1,42 @@
-from Bio.SeqUtils import MeltingTemp as mt
-from Bio.Seq import Seq
 import tkinter as tk
 from tkinter import messagebox
+from Bio.SeqUtils import MeltingTemp as mt
+from Bio.Seq import Seq
+import itertools
 
 MIXED_BASES = {'R': ['A', 'G'], 'Y': ['C', 'T'], 'S': ['G', 'C'], 'W': ['A', 'T'], 'K': ['G', 'T'], 'M': ['A', 'C'],
                'B': ['C', 'G', 'T'], 'D': ['A', 'G', 'T'], 'H': ['A', 'C', 'T'], 'V': ['A', 'C', 'G'], 'N': ['A', 'T', 'G', 'C']}
 
 def expand_sequence(seq):
-    expanded_seqs = [seq]
-    for mixed_base in MIXED_BASES:
-        new_seqs = []
-        for seq in expanded_seqs:
-            if mixed_base in seq:
-                for base in MIXED_BASES[mixed_base]:
-                    new_seqs.append(seq.replace(mixed_base, base, 1))
-            else:
-                new_seqs.append(seq)
-        expanded_seqs = new_seqs
-    return expanded_seqs
+    seq = seq.upper()
+    for base in MIXED_BASES:
+        if base in seq:
+            return list(itertools.product(*[MIXED_BASES[base] if b == base else b for b in seq]))
+    return [seq]
 
 def calculate_tm_and_annealing():
     primer1 = primer1_entry.get()
     primer2 = primer2_entry.get()
+
     expanded_primers1 = expand_sequence(primer1)
     expanded_primers2 = expand_sequence(primer2)
 
+    tm1_values = [mt.Tm_NN(Seq(''.join(primer))) for primer in expanded_primers1]
+    tm2_values = [mt.Tm_NN(Seq(''.join(primer))) for primer in expanded_primers2]
+
+    avg_tm1 = sum(tm1_values) / len(tm1_values)
+    avg_tm2 = sum(tm2_values) / len(tm2_values)
+
+    avg_annealing_temp1 = avg_tm1 - 5
+    avg_annealing_temp2 = avg_tm2 - 5
+
     results = ""
-    for primer1 in expanded_primers1:
-        for primer2 in expanded_primers2:
-            tm1 = mt.Tm_NN(Seq(primer1))
-            tm2 = mt.Tm_NN(Seq(primer2))
-            annealing_temp1 = tm1 - 5
-            annealing_temp2 = tm2 - 5
-            results += f"Primer1: {primer1}, Tm: {tm1}, Annealing Temperature: {annealing_temp1}\n"
-            results += f"Primer2: {primer2}, Tm: {tm2}, Annealing Temperature: {annealing_temp2}\n"
+    for i, primer in enumerate(expanded_primers1):
+        results += f"Primer1: {''.join(primer)}, Tm: {tm1_values[i]}, Annealing Temperature: {tm1_values[i] - 5}\n"
+    for i, primer in enumerate(expanded_primers2):
+        results += f"Primer2: {''.join(primer)}, Tm: {tm2_values[i]}, Annealing Temperature: {tm2_values[i] - 5}\n"
+    results += f"Primer1 Average Tm: {avg_tm1}, Average Annealing Temperature: {avg_annealing_temp1}\n"
+    results += f"Primer2 Average Tm: {avg_tm2}, Average Annealing Temperature: {avg_annealing_temp2}\n"
     messagebox.showinfo("Results", results)
 
 root = tk.Tk()
